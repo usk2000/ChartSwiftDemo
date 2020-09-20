fed2aebb58a099593142c1830b154eae74dfc8bc
Yuusuke Hasegawa <31399699+usk2000@users.noreply.github.com>
2020-09-20 21:34:59 +0900

    Initial Commit

---
+++ ChartSwiftDemo/ChartSwiftDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  ChartSwiftDemoApp.swift
+//  ChartSwiftDemo
+//
+//  Created by Yusuke Hasegawa on 2020/09/20.
+//
+
+import SwiftUI
+
+@main
+struct ChartSwiftDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ ChartSwiftDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  ChartSwiftDemo
+//
+//  Created by Yusuke Hasegawa on 2020/09/20.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
