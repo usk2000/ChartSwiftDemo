a26159b64067de51613f56b96dc5388a12042b44
Yuusuke Hasegawa <31399699+usk2000@users.noreply.github.com>
2020-10-06 23:09:31 +0900

    feat: change to BarChart

--- ChartSwiftDemo/ContentView.swift
+++ ChartSwiftDemo/ContentView.swift
@@ -9,16 +9,16 @@ import SwiftUI
 import ChartSwift
 
 struct ContentView: View {
+    
+    var items: [ChartItem] = [
+        .init(title: "タイトル", value: 0.8, text: "50%", color: .green),
+        .init(title: "タイトル２", value: 0.5, text: "30%", color: .blue),
+        .init(title: "タイトル３", value: 0.1, text: "10%", color: .blue)
+    ]
+    
     var body: some View {
-        VStack(spacing: 16) {
-            Text("Hello, world!")
-                .padding()
-            BarRow(item: .init(title: "タイトル", value: 0.8, text: "50%", color: .green), labelWidth: 65)
-                .padding(.horizontal, 16)
-            BarRow(item: .init(title: "タイトル２", value: 0.5, text: "30%", color: .blue), labelWidth: 65)
-                .padding(.horizontal, 16)
-            BarRow(item: .init(title: "タイトル３", value: 0.1, text: "10%", color: .blue), labelWidth: 65)
-                .padding(.horizontal, 16)
+        VStack {
+            BarChart.init(items: items)
             Spacer()
         }
         
@@ -29,5 +29,7 @@ struct ContentView_Previews: PreviewProvider {
     static var previews: some View {
         ContentView()
             .environment(\.barBackground, Color.init(white: 0.95))
+            .environment(\.barHeight, 30)
+            .environment(\.labelWidth, 65)
     }
 }
